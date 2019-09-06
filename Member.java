f45045ffe90d9d8e3ea7566366e5eb7907b9378e
Thiago Almeida <43508221+AL-Theo@users.noreply.github.com>
2019-09-06 10:39:34 -0400

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,68 @@
+package gymMembership;
+
+
+//Parent class that constructs a member
+public class Member {
+
+	private char memberType;
+	private int memberID;
+	private String name;
+	private double fees;
+	
+
+	public Member(char pMemberType, int pMemberID, String pName, double pFees) {
+		
+		memberType = pMemberType;
+		memberID = pMemberID;
+		name = pName;
+		fees = pFees;
+	}
+
+
+	public char getMemberType() {
+		return memberType;
+	}
+
+
+	public void setMemberType(char pMemberType) {
+		memberType = pMemberType;
+	}
+
+
+	public int getMemberID() {
+		return memberID;
+	}
+
+
+	public void setMemberID(int pMemberID) {
+		memberID = pMemberID;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String pName) {
+		name = pName;
+	}
+
+
+	public double getFees() {
+		return fees;
+	}
+
+
+	public void setFees(double pFees) {
+		fees = pFees;
+	}
+	
+	
+	@Override
+	public String toString(){
+		return memberType + ", " + memberID + ", " + name + ", " + ", " + fees;
+	}
+}
+
+
